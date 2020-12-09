8b990ca0f6439a4e0b3f04eff4952af4c31b8f1c
Michael Lan <michaellan202@gmail.com>
2020-12-09 12:59:11 -0800

    command line args

---
+++ getargs.py
@@ -0,0 +1,13 @@
+import argparse
+
+def getargs():
+    parser = argparse.ArgumentParser(description='USACO Submission Client')
+    parser.add_argument('--fresh', help='invalidate cookies and force fresh login', action='store_true')
+    parser.add_argument('-f', '--file', help='submission file', action='store_true')
+    parser.add_argument('-l', '--lang', help='submission language', default='infer')
+    parser.add_argument(dest='problem', help='USACO problem link or cpid')
+    args = parser.parse_args()
+    print(args)
+
+if __name__ == '__main__':
+    getargs()
