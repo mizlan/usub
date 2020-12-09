5d574ac77042dbe4025babcdecdd548b3b245cd8
Michael Lan <michaellan202@gmail.com>
2020-12-09 14:46:33 -0800

    added status

--- status.py
+++ status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 import json
+import time
+import sys
 
 import requests
 from bs4 import BeautifulSoup
+from colorama import Fore, Back, Style
+from alive_progress import alive_bar
 
 import session
 from submitutil import SubmissionID
@@ -14,19 +18,42 @@ def get_status(sid: SubmissionID):
     data = {'sid': sid}
     cookies = session.get_cookie_dict()
 
-    response = requests.post(
-        url,
-        data=data,
-        cookies=cookies
-    )
+    respdata = None
+    attempts = 0
+    max_iterations = 40
+    spinner = ' ◢', '◣ ', '◤ ', ' ◥'
 
-    data = json.loads(response.text)
-    soup = BeautifulSoup(data['jd'], 'lxml')
+    with alive_bar(max_iterations, spinner='dots_waves') as bar:
+        while True:
+            attempts += 1
+            response = requests.post(
+                url,
+                data=data,
+                cookies=cookies
+            )
+            respdata = json.loads(response.text)
+            status = respdata['sr']
+            # print(spinner[attempts % 4], end='\r')
+            if int(respdata['cd']) > -8:
+                break
+            bar()
+            time.sleep(0.200)
 
+    # print(respdata)
+    if respdata['jd'].strip() == '':
+        print(Fore.RED, end='')
+        print(respdata['sr'].strip())
+        print(respdata['output'].strip())
+        print(Style.RESET_ALL)
+        raise IOError('Error on submission.')
+    soup = BeautifulSoup(respdata['jd'], 'lxml')
     return soup
 
 def display_status(sid: SubmissionID):
-    soup = get_status(sid)
+    try:
+        soup = get_status(sid)
+    except IOError:
+        sys.exit(1)
 
     testcases = []
     for i in soup.find_all('a', class_='masterTooltip'):
