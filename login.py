e68bf40e018aa9b774aa8180fe77e4f4518a6276
Michael Lan <michaellan202@gmail.com>
2020-12-09 11:23:22 -0800

    login management

--- login.py
+++ login.py
@@ -1,3 +1,19 @@
+import json, re
+
 import requests
 
+def login(username: str, password: str) -> str:
+    url = 'http://usaco.org/current/tpcm/login-session.php'
+    data = { 'uname': username, 'password': password }
+    response = requests.post(
+        url,
+        data=data
+    )
+    headers = response.headers
+    data = json.loads(response.text)
+    if data['msg'] == 'Incorrect password':
+        raise KeyError('incorrect password!')
+    return re.match(r'PHPSESSID=(\S+);', headers['Set-cookie']).group(1)
 
+if __name__ == '__main__':
+    print(login('test', 'test'))
